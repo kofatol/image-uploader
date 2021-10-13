4c8ab4e69a09e4b731bc1444570c85a0c96333d0
kcherkasov <kcherkasov@emphasoft.com>
2021-10-13 20:43:40 +0300

    image uploading functionality created in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React from "react";
-import "./App.css";
+import './App.scss';
+import { getImageSrc } from './App.model';
+import React, { ChangeEvent, useState } from 'react';
 
-const App: React.FC = () => {
+function App() {
+  const initState = {name: '', src: ''};
+  const [imageState, setImageState] = useState(initState);
+
+  const onUploadImageClick = ({currentTarget}: React.MouseEvent<HTMLButtonElement>) => {
+    const fileInput = currentTarget.nextElementSibling as HTMLInputElement;
+    fileInput.click();
+  };
+
+  const onFileInputClick = async ({target}: ChangeEvent<HTMLInputElement>) => {
+    const file: File = (target.files as FileList)[0];
+
+    if (file !== undefined) {
+      const imageSrc = await getImageSrc(file) as string;
+
+      setImageState({
+        name: file.name,
+        src: imageSrc
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      Hello Veeam!
+    <div className='App'>
+      <div className='img-upload-group'>
+        <button
+          className='img-upload-group__button'
+          onClick={onUploadImageClick}
+        >
+          Upload image
+        </button>
+        <input
+          type='file'
+          name='image'
+          value=''
+          accept='image/*'
+          className='img-upload-group__input'
+          onChange={onFileInputClick}
+        />
+      </div>
     </div>
   );
-};
+}
 
 export default App;
