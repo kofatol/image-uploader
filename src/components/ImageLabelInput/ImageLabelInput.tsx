9cee5be5208ab66d404924ac788b790d29b880bf
kcherkasov <kcherkasov@emphasoft.com>
2021-10-14 00:00:10 +0300

    ImageLabel component and all its files created

---
+++ src/components/ImageLabelInput/ImageLabelInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+type ImageLabelProps = {
+  style: {
+    top: number;
+    left: number;
+  }
+}
+
+export default function ImageLabel({style}: ImageLabelProps) {
+  const [inputValue, setInputValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+  const [isInputEmpty, setIsInputEmpty] = useState(false);
+
+  const onInputChange = ({target: {value}}: React.ChangeEvent<HTMLInputElement>) => setInputValue(value);
+
+  const onInputEnd = (): void => {
+    setIsTouched(true);
+    !inputValue && setIsInputEmpty(true);
+  };
+
+  return (
+    isInputEmpty ? null :
+      <div
+        style={style}
+        className='ImageLabel'
+      >
+        <input
+          autoFocus
+          maxLength={100}
+          value={inputValue}
+          disabled={isTouched}
+          size={inputValue.length}
+          onChange={onInputChange}
+          onBlur={onInputEnd}
+          className='ImageLabel__input'
+        />
+      </div>
+  );
+}
