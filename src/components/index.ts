962d9e4356377bb1291c88c05b4c16bd2972a546
kcherkasov <kcherkasov@emphasoft.com>
2021-10-14 23:51:57 +0300

    components/index.js file code style fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export {default as ImageLabel} from "./ImageLabel";
-export {default as ImageContent} from "./ImageContent";
-export {default as ImageUploader} from "./ImageUploader";
+export { default as ImageLabel } from './ImageLabel';
+export { default as ImageContent } from './ImageContent';
+export { default as ImageUploader } from './ImageUploader';
