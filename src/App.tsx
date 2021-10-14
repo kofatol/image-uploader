e39998ed8a59b4803bf17fc612f998e6053f8a83
kcherkasov <kcherkasov@emphasoft.com>
2021-10-14 23:20:10 +0300

    useImageHandler hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import 'App.scss';
-import { useState } from 'react';
-import { getImageSrc } from 'App.model';
 import ImageContent from 'components/ImageContent';
 import ImageUploader from 'components/ImageUploader';
-import { onFileInputClickEvent, onUploadImageClickEvent } from 'model/types';
+import useImageHandler from 'hooks/useImageHandler';
 
 export default function App() {
-  const [imageSrc, setImageSrc] = useState("");
-
-  const onUploadImageClick = ({currentTarget}: onUploadImageClickEvent) => {
-    const fileInput = currentTarget.nextElementSibling as HTMLInputElement;
-    fileInput.click();
-  };
-
-  const onFileInputClick = async ({target}: onFileInputClickEvent) => {
-    const file: File = (target.files as FileList)[0];
-
-    if (file !== undefined) {
-      const imageSrc = await getImageSrc(file) as string;
-      setImageSrc(imageSrc);
-    }
-  };
+  const {imageSrc, onUploadImageClick, onFileInputClick} = useImageHandler();
 
   return (
     <div className='App'>
---
+++ src/hooks/useImageHandler.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { getImageSrc } from 'App.model';
+import { onFileInputClickEvent, onUploadImageClickEvent } from '../model/types';
+
+const useImageHandler = () => {
+  const [imageSrc, setImageSrc] = useState('');
+
+  const onUploadImageClick = ({currentTarget}: onUploadImageClickEvent) => {
+    const fileInput = currentTarget.nextElementSibling as HTMLInputElement;
+    fileInput.click();
+  };
+
+  const onFileInputClick = async ({target}: onFileInputClickEvent) => {
+    const file: File = (target.files as FileList)[0];
+
+    if (file !== undefined) {
+      const imageSrc = await getImageSrc(file) as string;
+      setImageSrc(imageSrc);
+    }
+  };
+
+  return {imageSrc, onUploadImageClick, onFileInputClick};
+};
+
+export default useImageHandler;
