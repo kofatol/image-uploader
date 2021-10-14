aa2fe1c0569f023e0dc412ec278553c27bb7d37e
kcherkasov <kcherkasov@emphasoft.com>
2021-10-15 00:10:06 +0300

    ImageUploader code style fix

--- src/components/ImageUploader/ImageUploader.tsx
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -2,8 +2,8 @@ import './ImageUploader.scss';
 import { onFileInputClickEvent, onUploadImageClickEvent } from 'model/types';
 
 type ImageUploaderProps = {
-  onUploadImageClick(e: onUploadImageClickEvent): void;
   onFileInputClick(e: onFileInputClickEvent): void;
+  onUploadImageClick(e: onUploadImageClickEvent): void;
 }
 
 export default function ImageUploader({onUploadImageClick, onFileInputClick}: ImageUploaderProps) {
@@ -11,18 +11,18 @@ export default function ImageUploader({onUploadImageClick, onFileInputClick}: Im
     <div className='ImageUploader'>
       <h3 className='ImageUploader__text'>To upload an image please click on the button below</h3>
       <button
-        className='ImageUploader__button'
         onClick={onUploadImageClick}
+        className='ImageUploader__button'
       >
         Upload image
       </button>
       <input
+        value=''
         type='file'
         name='image'
-        value=''
         accept='image/*'
-        className='ImageUploader__input'
         onChange={onFileInputClick}
+        className='ImageUploader__input'
       />
     </div>
   );
